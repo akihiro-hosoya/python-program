05b259eccc3a8d64b13c9d8f103d1624931504be
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-08-01 22:07:01 +0900

    投稿機能

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 class IndexView(View):
@@ -16,3 +19,27 @@ class PostDetailView(View):
         return render(request, 'blog/post_detail.html', {
 		'post_data' : post_data
         })
+
+class CreatePostView(View):
+    def get(self, request, *args, **kwargs):
+        form = PostForm(request.POST or None)
+        return render(request, 'blog/post_form.html', {
+            'form': form
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = PostForm(request.POST or None)
+
+        if form.is_vaild():
+            post_data = Post()
+            post_data.author = request.user
+            post_data.title = form.cleaned_data['title']
+            post_data.content = form.cleaned_data['content']
+            post_data.save()
+            return redirect('post_detail', post_data.id)
+            
+
+        return render(request, 'blog/post_form.html', {
+            'form': form
+        })
+        
