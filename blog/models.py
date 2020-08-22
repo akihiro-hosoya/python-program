e73c8f9ea6db25be3f7a960c8d9955ed30518ce4
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-08-22 13:29:15 +0900

    カテゴリ機能追加

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,15 @@ from django.conf import settings
 from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField("カテゴリ", max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
     title = models.CharField("タイトル", max_length=200)
     image = models.ImageField(upload_to='images', verbose_name='イメージ画像', null=True, blank=True)
     content = models.TextField("本文")
